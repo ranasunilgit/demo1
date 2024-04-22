a32c4e5dbc5b40cc64b8cf9b18024004ace32bbb
Sunil Rana <sunilmaxout21@gmail.com>
2024-04-22 12:02:39 +0530

    2nd change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React New
+          Learn React New New
         </a>
       </header>
     </div>
